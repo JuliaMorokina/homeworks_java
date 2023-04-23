63b5ced69b2318b2247a3b5dbd16f8f78a460bde
juliamorokina <juliamorokina@mail.ru>
2023-04-23 21:03:39 +0300

    homework Базовый синтаксис Java

---
+++ src/HomeWork.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class HomeWork {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Расскажите о вашем друге");
+        System.out.println("Как зовут вашего друга?");
+
+        String name = scanner.nextLine();
+        System.out.println("Сколько лет вашему другу?");
+
+        String age = scanner.nextLine();
+        System.out.println("Моему другу " + name + " сейчас " + age + " лет");
+    }
+}
